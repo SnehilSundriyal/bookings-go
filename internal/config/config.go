b0b07fac6d81ab3cc3f2254932ca6b83e2da70f4
SnehilSundriyal <snehilsundriyal@gmail.com>
2024-07-07 10:10:51 +0530

    refactoring-code

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,8 +8,8 @@ import (
 
 // AppConfig holds the application config
 type AppConfig struct {
-	UseCache 		bool
-	TemplateCache 	map[string]*template.Template
-	InProduction 	bool
-	Session 		*scs.SessionManager
-}	
+	UseCache      bool
+	TemplateCache map[string]*template.Template
+	InProduction  bool
+	Session       *scs.SessionManager
+}
